5c4ce092c6f2eeaf265eed35e68df9d1966a2178
Les Aker <me@lesaker.org>
2024-04-07 12:48:00 -0400

    fix typo

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 
 	d := mux.NewDispatcher(
 		mux.NewRoute(randomRegex, randomHandler),
-		mux.NewRoute(faviconRegex, faviconHanlder),
+		mux.NewRoute(faviconRegex, faviconHandler),
 		mux.NewRoute(indexRegex, indexHandler),
 	)
 	mux.Start(d)
